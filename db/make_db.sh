c5ba2513d9c4263838419d4903d21d1f0c42be0e
Simon Dietz <simon.dietz@student.hpi.de>
2020-05-17 19:29:17 +0200

    add google maps

--- db/make_db.sh
+++ db/make_db.sh
@@ -6,7 +6,7 @@ sqlite3 plantwatch.db  "CREATE TABLE addresses(blockid TEXT NOT NULL PRIMARY KEY
 
 sqlite3 plantwatch.db  "CREATE TABLE blocks(plantid TEXT, blockid TEXT NOT NULL PRIMARY KEY, blockdescription TEXT, federalstate TEXT, energysource TEXT, initialop INTEGER, chp TEXT, blockname TEXT, netpower REAL, state TEXT, endop TEXT, company TEXT, FOREIGN KEY (blockid) REFERENCES addresses(blockid) ON DELETE CASCADE);"
 
-sqlite3 plantwatch.db  "CREATE TABLE plants(plantid TEXT NOT NULL PRIMARY KEY, plantname TEXT, federalstate TEXT, energysource TEXT, chp TEXT, latestexpanded INT, initialop INT, totalpower REAL, state TEXT, blockcount INT,  company TEXT, FOREIGN KEY (plantid) REFERENCES blocks(plantid) ON DELETE CASCADE);"
+sqlite3 plantwatch.db  "CREATE TABLE plants(plantid TEXT NOT NULL PRIMARY KEY, plantname TEXT, federalstate TEXT, energysource TEXT, chp TEXT, latestexpanded INT, initialop INT, totalpower REAL, state TEXT, blockcount INT,  company TEXT, plz TEXT, place TEXT, street TEXT, number TEXT, latitude REAL, longitude REAL, FOREIGN KEY (plantid) REFERENCES blocks(plantid) ON DELETE CASCADE);"
 
 sqlite3 plantwatch.db "CREATE TABLE power(powerid INTEGER NOT NULL PRIMARY KEY, producedat TIMESTAMP NOT NULL, blockid TEXT NOT NULL, power INTEGER NOT NULL, FOREIGN KEY (blockid) REFERENCES blocks(blockid) ON DELETE CASCADE);"
 
@@ -17,4 +17,4 @@ sqlite3 plantwatch.db  "CREATE TABLE pollutions(pollutionsid INTEGER NOT NULL, y
 echo -e '.separator "," \n.import stammdaten_nh.csv addresses  \n.import plants_nh.csv plants \n.import blocks_nh.csv blocks\n. import produced_power_pg.csv power\n. import monthly.csv month \n.import pollutants_pg.csv pollutions ' | sqlite3 plantwatch.db
 
 # 	year 	plantid 	pollutant 	releases_to 	amount 	potency 	unit_2 	amount_2
-
+# plz 	ort 	strasse 	hausnr 	geo_lat_wgs84 	geo_long_wgs84
