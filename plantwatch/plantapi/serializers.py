8977047ca72ca695dd9d66d722868c1a8c9e68ff
dietzi96 <simon.dietz@plantwatch.de>
2025-02-13 17:31:42 +0100

    improve plantapi

--- plantwatch/plantapi/serializers.py
+++ plantwatch/plantapi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from plantmaster.models import Blocks, Plants
+from plantmaster.models import Blocks, Plants, Pollutions
 
 class BlockSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,17 @@ class PlantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Plants
         fields = ['plantid', 'plantname', 'blockcount', 'totalpower', 'energysource', 'state', 'chp', 'initialop', 'latestexpanded', 'company', 'blocks']
+
+class PollutantSerializer(serializers.HyperlinkedModelSerializer):
+    #pollutants = PollutantSerializer(many=True)
+
+    class Meta:
+        model = Pollutions
+        fields = ['pollutionsid', 'year', 'plantid', 'pollutant', 'amount']
+
+class PollutionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Pollutions
+        fields = ['pollutionsid', 'plantid', 'year', 'pollutant', 'amount', 'amount2', 'unit2']
+
--- plantwatch/plantapi/urls.py
+++ plantwatch/plantapi/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
-from .views import BlockViewSet, PlantViewSet
+from .views import BlockViewSet, PlantViewSet, PollutionViewSet, PollutionList, PollutionList2
 
 router = routers.DefaultRouter()
 router.register(r'plants', PlantViewSet)
 router.register(r'blocks', BlockViewSet)
+router.register(r'pollutions', PollutionViewSet)
+#router.register(r'', PollutionList.as_view(), basename='pollutant')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth', include('rest_framework.urls')),
+    re_path('^pollution/by_plantid/(?P<plantid>.+)/', PollutionList.as_view()),
+    re_path('^pollution/by_year/(?P<year>.+)/', PollutionList2.as_view())
 ]
 
 '''
